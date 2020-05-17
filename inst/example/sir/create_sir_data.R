12b3718bbfa079b51fb73fa68ea06b44f6a82737
Lilith Whittles <lilithwhittles@gmail.com>
2020-05-17 11:00:23 +0100

    fix formatting

--- inst/example/sir/create_sir_data.R
+++ inst/example/sir/create_sir_data.R
@@ -11,8 +11,8 @@ par(mfrow = c(1, 2), bty = "n", mar = c(3, 3, 1, 1), mgp = c(1.5, 0.5, 0))
 plot(tmp[, "incid"], type = "l", ylab = "incidence", xlab = "day")
 cols <- 1:3
 comps <- c("S", "I", "R")
-matplot(tmp[, comps], type= "l", lty = 1, 
-        ylab = "number of people", 
+matplot(tmp[, comps], type = "l", lty = 1,
+        ylab = "number of people",
         xlab = "day")
 legend("topright", legend = comps, fill = cols, bty = "n", ncol = 3)
 
