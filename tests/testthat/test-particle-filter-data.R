4b88bdf84d05d1ad7c02968f13260e1f1c950993
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-05-19 13:28:21 +0100

    Add simple test of particle_filter_data

--- tests/testthat/test-particle-filter-data.R
+++ tests/testthat/test-particle-filter-data.R
@@ -16,3 +16,17 @@ test_that("particle filter data validates time", {
     particle_filter_data(d + 0.5, "t", 10),
     "'t' must be an integer")
 })
+
+
+test_that("particle filter data creates data", {
+  d <- data.frame(day = 0:10, data = 1:11)
+  res <- particle_filter_data(d, "day", 10)
+  expect_setequal(
+    names(res),
+    c("day_start", "day_end", "step_start", "step_end", "data"))
+  expect_equal(res$day_start, 0:9)
+  expect_equal(res$day_end, 1:10)
+  expect_equal(res$step_start, 0:9 * 10)
+  expect_equal(res$step_end, 1:10 * 10)
+  expect_equal(res$data, 2:11)
+})
