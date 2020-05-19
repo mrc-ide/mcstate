70463696c223fd849bb9d248f8998157b75c075a
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-05-19 10:49:15 +0100

    New utilities

--- tests/testthat/test-utils-assert.R
+++ tests/testthat/test-utils-assert.R
@@ -4,3 +4,20 @@ test_that("assert_is", {
   expect_error(assert_is("x", "foo"), "must be a foo")
   expect_silent(assert_is(structure("x", class = "foo"), "foo"))
 })
+
+
+test_that("assert_integer", {
+  expect_error(assert_integer(pi), "'pi' must be an integer")
+  expect_silent(assert_integer(1L))
+  expect_silent(assert_integer(1))
+  expect_silent(assert_integer(1 + 1e-15))
+})
+
+
+test_that("assert_strictly_increasing", {
+  expect_silent(assert_strictly_increasing(c(0, 1, 2)))
+  expect_error(assert_strictly_increasing(c(0, 0, 1)),
+               "must be strictly increasing")
+  expect_error(assert_strictly_increasing(c(0, -1, -2)),
+               "must be strictly increasing")
+})
