e84a8cb55151e63c31c900e73b2651ba7e96f8b7
John Lees <lees.john6@gmail.com>
2020-06-22 21:32:49 +0100

    Update MCMC plotting and summary functions

--- R/pmcmc.R
+++ R/pmcmc.R
@@ -2,82 +2,45 @@
 ##'
 ##' @title Run a pmcmc sampler
 ##'
-##' @param data Data to fit to.  This must be constructed with
-##'   \code{particle_filter_data}
+##' @param mcmc_range \code{data.frame} detailing parameters to sample.
+##' Must contain columns 'name' (parameter names), 'init' (initial values),
+##' 'min' (minimum values), 'max' (maximum values),
+##' 'discrete (boolean indicating whether a discrete quantity)' and
+##' 'target'
 ##'
-##' @param model_params Model parameters, from a call to
-##'   \code{generate_parameters()}.
-##'
-##' @param sircovid_model An odin model generator and comparison function.
-
-##' @param pars_obs list of parameters to use in comparison
-##'   with \code{compare_icu}. Must be a list containing, e.g.
-##'   list(phi_general = 0.95,
-##'   k_general = 2,
-##'   phi_ICU = 0.95,
-##'   k_ICU = 2,
-##'   phi_death = 926 / 1019,
-##'   k_death = 2,
-##'   exp_noise = 1e6)
-##'
-##' @param n_mcmc number of mcmc mcmc iterations to perform
-##'
-##' @param pars_to_sample \code{data.frame} detailing parameters to sample. Must contain columns
-##'   'names' (parameter names), 'init' (initial values), 'min' (minimum values), 'max' (maximum values),
-##'   'discrete (boolean indicating whether a discrete quantity)'
-##'
-##' @param pars_lprior functions to calculate log prior for each parameter. A named list for each parameter
-##'   listed in \code{pars_to_sample}. Each value must be a function which takes named parameter vector as
+##' @param lprior_funcs functions to calculate log prior for each parameter.
+##' A named list for each parameter listed in \code{pars_to_sample}.
+##' Each value must be a function which takes named parameter vector as
 ##'   input, returns a single numeric which is log of the prior probability.
 ##'
-##' @param proposal_kernel named matrix of proposal covariance for parameters
+##' @param filter A particle filter to sample
 ##'
 ##' @param n_particles Number of particles
 ##'
-##' @param steps_per_day Number of steps per day
+##' @param proposal_kernel named matrix of proposal covariance for parameters
 ##'
-##' @param output_proposals Logical indicating whether proposed parameter jumps should be output along with results
+##' @param run_params List of parameters for \code{particle_filter$run}
+##'
+##' @param output_proposals Logical indicating whether proposed parameter
+##' jumps should be output along with results
 ##'
 ##' @param n_chains Number of chains to run
 ##'
 ##' @return an mcmc object containing
 ##' - List of inputs
 ##' - Matrix of accepted parameter samples, rows = iterations
-##'   as well as log prior, (particle filter estimate of) log likelihood and log posterior
+##'   as well as log prior, (particle filter estimate of) log likelihood
+##'   and log posterior
 ##'
-##' @description The user inputs initial parameter values for beta_start and sample_date
-##' The log prior likelihood of these parameters is calculated based on the user-defined
-##' prior distributions.
-##' The log likelihood of the data given the initial parameters is estimated using a particle filter,
-##' which has two functions:
-##'      - Firstly, to generate a set of 'n_particles' samples of the model state space,
-##'        at time points corresponding to the data, one of which is
-##'        selected randomly to serve as the proposed state sequence sample at the final
-##'        data time point.
-##'      - Secondly, to produce an unbiased estimate of the likelihood of the data given the proposed parameters.
-##' The log posterior of the initial parameters given the data is then estimated by adding the log prior and
-##' log likelihood estimate.
-##'
-##' The pMCMC sampler then proceeds as follows, for n_mcmc iterations:
-##' At each loop iteration the pMCMC sampler perfsorms three steps:
-##'   1. Propose new candidate samples for beta_start, beta_end and start_date based on
-##'     the current samples, using the proposal distribution
-##'     (currently multivariate Gaussian with user-input covariance matrix (proposal_kernel), and reflecting boundaries defined by pars_min, pars_max)
-##'   2. Calculate the log prior of the proposed parameters,
-##'      Use the particle filter to estimate log likelihood of the data given the proposed parameters, as described above,
-##'      as well as proposing a model state space.
-##'      Add the log prior and log likelihood estimate to estimate the log posterior of the proposed parameters given the data.
-##'   3. Metropolis-Hastings step: The joint canditate sample (consisting of the proposed parameters
-##'      and state space) is then accepted with probability min(1, a), where the acceptance ratio is
-##'      simply the ratio of the posterior likelihood of the proposed parameters to the posterior likelihood
-##'      of the current parameters. Note that by choosing symmetric proposal distributions by including
-##'      reflecting boundaries, we avoid the the need to include the proposal likelihood in the MH ratio.
-##'
-##'   If the proposed parameters and states are accepted then we update the current parameters and states
-##'   to match the proposal, otherwise the previous parameters/states are retained for the next iteration.
+##' @description This is a basic Metropolis-Hastings MCMC sampler.
+##' The \code{filter} is run with a set of parameters to evaluate the
+##' likeihood. A new set of parameters is proposed, and these likelihoods are
+##' compared, jumping with probability equal to their ratio. This is repeated
+##' for \code{n_mcmc} proposals, \code{n_chains} independent times.
 ##'
 ##' @export
 ##' @import coda
+##' @import furrr
 ##' @importFrom stats rnorm
 ##' @importFrom mvtnorm rmvnorm
 ##' @importFrom graphics matplot
@@ -153,55 +116,7 @@ pmcmc <- function(mcmc_range,
   res
 }
 
-mcmc_validate_range <- function(range) {
-  assert_is(range, "data.frame")
-  msg <- setdiff(c("name", "init", "min", "max", "discrete", "target"),
-                   names(range))
-  if (length(msg) > 0L) {
-    stop("Missing columns from 'mcmc_range': ",
-           paste(squote(msg), collapse = ", "))
-  }
-
-  if (anyDuplicated(range$name)) {
-    stop("Duplicate 'name' entries not allowed in 'mcmc_range'")
-  }
-
-  targets <- c("step_start", "model_data", "pars_compare")
-  err <- setdiff(range$target, targets)
-  if (length(err) > 0L) {
-    stop(sprintf("Invalid target %s: must be one of %s",
-                 paste(squote(err), collapse = ", "),
-                 paste(squote(targets), collapse = ", ")))
-  }
-
-  index <- lapply(targets, function(t) which(range$target == t))
-  names(index) <- targets
-  if (length(index$step_start) > 1L) {
-    stop("At most one target may be 'step_start'")
-  }
-
-  if (!is.logical(range$discrete)) {
-    stop("'discrete' entries must be TRUE or FALSE")
-  }
-  if (!is.numeric(range$init)) {
-    stop("'init' entries must be numeric")
-  }
-  if (!is.numeric(range$min)) {
-    stop("'min' entries must be numeric")
-  }
-  if (!is.numeric(range$max)) {
-    stop("'max' entries must be numeric")
-  }
-
-  if(any(range$init < range$min | range$init > range$mmax)) {
-    stop('initial parameters are outside of specified range')
-  }
-
-  list(range = range,
-       index = index)
-}
-
-# Run a single pMCMC chain
+# Run a single pMCMC chain (can be done in parallel)
 run_mcmc_chain <- function(n_mcmc,
                            vars,
                            lprior_funcs,
@@ -315,8 +230,8 @@ run_mcmc_chain <- function(n_mcmc,
   rejection_rate <- coda::rejectionRate(coda_res)
   ess <- coda::effectiveSize(coda_res)
 
-  out <- list('results' = as.data.frame(res),
-              'acceptance_rate' = 1-rejection_rate,
+  out <- list("results" = as.data.frame(res),
+              "acceptance_rate" = 1-rejection_rate,
               "ess" = ess)
 
   if (output_proposals) {
@@ -358,9 +273,59 @@ reflect_proposal <- function(x, floor, cap) {
   abs((x + interval - floor) %% (2 * interval) - interval) + floor
 }
 
+# Check input data.frame
+mcmc_validate_range <- function(range) {
+  assert_is(range, "data.frame")
+  msg <- setdiff(c("name", "init", "min", "max", "discrete", "target"),
+                   names(range))
+  if (length(msg) > 0L) {
+    stop("Missing columns from 'mcmc_range': ",
+           paste(squote(msg), collapse = ", "))
+  }
+
+  if (anyDuplicated(range$name)) {
+    stop("Duplicate 'name' entries not allowed in 'mcmc_range'")
+  }
+
+  targets <- c("step_start", "model_data", "pars_compare")
+  err <- setdiff(range$target, targets)
+  if (length(err) > 0L) {
+    stop(sprintf("Invalid target %s: must be one of %s",
+                 paste(squote(err), collapse = ", "),
+                 paste(squote(targets), collapse = ", ")))
+  }
+
+  index <- lapply(targets, function(t) which(range$target == t))
+  names(index) <- targets
+  if (length(index$step_start) > 1L) {
+    stop("At most one target may be 'step_start'")
+  }
+
+  if (!is.logical(range$discrete)) {
+    stop("'discrete' entries must be TRUE or FALSE")
+  }
+  if (!is.numeric(range$init)) {
+    stop("'init' entries must be numeric")
+  }
+  if (!is.numeric(range$min)) {
+    stop("'min' entries must be numeric")
+  }
+  if (!is.numeric(range$max)) {
+    stop("'max' entries must be numeric")
+  }
+
+  if(any(range$init < range$min | range$init > range$mmax)) {
+    stop('initial parameters are outside of specified range')
+  }
+
+  list(range = range,
+       index = index)
+}
+
 ##' @title create a master chain from a pmcmc_list object
 ##' @param x a pmcmc_list object
-##' @param burn_in an integer denoting the number of samples to discard from each chain
+##' @param burn_in an integer denoting the number of samples to discard
+##' from each chain
 ##' @export
 ##'
 create_master_chain <- function(x, burn_in) {
@@ -384,31 +349,22 @@ create_master_chain <- function(x, burn_in) {
   )
 
   master <- do.call(what = rbind, args = chains)
-  class(master) <- "mcstate_pmcmc_chain"
   master
 }
 
+#
+# Summary and plotting functions
+#
 
 ##' @export
 ##' @importFrom stats cor sd
-summary.pmcmc <- function(object, ...) {
+summary.mcstate_pmcmc <- function(object, ...) {
 
-  par_names <- names(object$inputs$pars$pars_init)
-
-  ## convert start_date to numeric to calculate stats
-  data_start_date <- sircovid_date(object$inputs$data$date[1])
-  traces <- object$results[,par_names]
-  traces$start_date <- sircovid_date(traces$start_date)
+  par_names <- colnames(object$results)
+  traces <- object$results
 
   # calculate correlation matrix
   corr_mat <- round(cor(traces),2)
-
-
-  # add in reduction in beta parameter
-  if('beta_end' %in% par_names) {
-    traces <- cbind(traces,
-                    beta_red = traces$beta_end/traces$beta_start)
-  }
 
   # compile summary
   summ <- rbind(mean = colMeans(traces),
@@ -419,95 +375,80 @@ summary.pmcmc <- function(object, ...) {
   summ <- as.data.frame(summ)
   summ <- round(summ, 3)
 
-
   sds <- round(apply(traces, 2, sd), 3)
   # convert start_date back into dates
-  summ$start_date <- sircovid_date_as_Date(summ$start_date)
 
   out <- list('summary' = summ,
               'corr_mat' = corr_mat,
               'sd' = sds)
   out
-
 }
 
 ##' @export
-summary.pmcmc_list <- function(object, ..., burn_in = 101) {
+summary.mcstate_pmcmc_list <- function(object, ..., burn_in = 1) {
 
   master_chain <- create_master_chain(x = object,
                                       burn_in = burn_in)
 
-  z <- list(inputs = object$inputs,
-            results = master_chain)
-  summary.pmcmc(z)
-
+  z <- list(results = master_chain)
+  summary.mcstate_pmcmc(z)
 }
 
+print_summ <- function(par_name, summ) {
+  x <- summ$summary
+  paste0(x['mean', par_name],
+          '\n(',
+          x['2.5%', par_name],
+          ', ',
+          x['97.5%', par_name], ')')
+}
 
 ##' @export
 ##' @importFrom viridis cividis
 ##' @importFrom graphics hist par plot.new text
-plot.pmcmc <- function(x, ...) {
+plot.mcstate_pmcmc <- function(x, ...) {
 
   summ <- summary(x)
-  par_names <- names(x$inputs$pars$pars_init)
+  par_names <- colnames(x$results)
+  n_pars <- length(par_names)
 
-  traces <- x$results[, par_names]
+  traces <- x$results
   cols <- viridis::cividis(nrow(traces))
   cols <- cols[order(order(x$results$log_likelihood))]
 
-
-
-  par_name <- 'beta_start'
-  print_summ <- function(par_name) {
-    x <- summ$summary
-    paste0(x['mean', par_name],
-           '\n(',
-           x['2.5%', par_name],
-           ', ',
-           x['97.5%', par_name], ')')
-  }
-
-
-
-  n_pars <- length(par_names)
-
-
-  par( bty = 'n',
-       mfcol = c(n_pars, n_pars + 1L),
-       mar = c(2.5,2.5,2,1.5),
-       mgp = c(1.5, 0.5, 0),
-       oma = c(1,1,1,1))
-
+  par(bty = "n",
+      mfcol = c(n_pars, n_pars + 1L),
+      mar = c(2.5,2.5,2,1.5),
+      mgp = c(1.5, 0.5, 0),
+      oma = c(1,1,1,1))
 
   for (i in seq_len(n_pars)) {
     for(j in seq_len(n_pars)) {
-
-      if (i == j) { # plot hists on diagonal
+      if (i == j) {
+        # plot hists on diagonal
         par_name <- par_names[i]
-        breaks = ifelse(par_name == 'start_date',
-                        yes = seq(as.Date('2019-12-01'),
-                                  as.Date(x$inputs$data$date[1]), 7),
-                        no = 10)
+        breaks = 10
         hist(traces[[i]],
-             main = print_summ(par_name),
+             main = print_summ(par_name, summ),
              xlab = par_name,
              breaks = breaks,
              cex.main = 1,
              font.main = 1,
              freq = FALSE)
-      } else if (i < j) {  # plot correlations on lower triangle
+      } else if (i < j) {
+        # plot correlations on lower triangle
         plot(x = traces[[i]],
              y = traces[[j]],
              xlab = par_names[i],
              ylab = par_names[j],
              col = cols,
              pch = 20)
-      } else if (i > j) { # print rho on upper triangle
+      } else if (i > j) {
+        # print rho on upper triangle
         plot.new()
         text(x = 0.5,
-             y=0.5,
-             labels = paste('r =',
+             y = 0.5,
+             labels = paste("r =",
                             summ$corr_mat[i, j]))
       }
     }
@@ -518,24 +459,20 @@ plot.pmcmc <- function(x, ...) {
          type = 'l',
          ylab = par_names,
          xlab = "Iteration")
-
-
 }
 
 ##' @export
 ##' @importFrom viridis cividis
 ##' @importFrom graphics hist par plot.new text lines legend
-##'
-plot.pmcmc_list <- function(x, burn_in = 1, ...) {
+plot.mcstate_pmcmc_list <- function(x, burn_in = 1, ...) {
 
   summ <- summary(x, burn_in = burn_in)
-  par_names <- names(x$inputs$pars$pars_init)
+  par_names <- colnames(x$chains[[1]]$results)
   n_pars <- length(par_names)
 
   chains <- x$chains
   n_chains <- length(chains)
   cols_trace <- rev(viridis::viridis(n_chains))
-
 
   # compile master chain and order by log posterior for plotting
   master_chain <- create_master_chain(x, burn_in = burn_in)
@@ -544,22 +481,11 @@ plot.pmcmc_list <- function(x, burn_in = 1, ...) {
   cols <- viridis::cividis(nrow(master_chain))
   cols <- cols[order(master_chain$log_posterior)]
 
-
-
-
   traces <- lapply(par_names, FUN = function(par_name) {
     lapply(X = chains,
            FUN = function(z) z$results[-seq_len(burn_in), par_name])
   })
   names(traces) <- par_names
-
-  plot_traces <- function(trace, col) {
-    lines(x = seq_along(trace),
-          y = trace,
-          col = col)
-  }
-
-
 
   breaks <- lapply(par_names, function(par_name){
     seq(from = min(master_chain[, par_name]),
@@ -578,49 +504,33 @@ plot.pmcmc_list <- function(x, burn_in = 1, ...) {
 
   hist_ylim <- lapply(hists, function(h) {
     chain_max <- sapply(h, function(chain) max(chain$density) )
-    c(0, max(chain_max))
+    upper_lim <- max(chain_max)
+    if (is.na(upper_lim)) {
+      upper_lim <- 0
+    }
+    c(0, upper_lim)
   })
 
-  plot_hists <- function(h, col, breaks) {
-    with(h, lines(x =  breaks,
-                  y = c(density,
-                        density[length(density)]),
-                  type = 's',
-                  col = col))
-  }
-
-
-  print_summ <- function(par_name) {
-    x <- summ$summary
-    paste0(x['mean', par_name],
-           '\n(',
-           x['2.5%', par_name],
-           ', ',
-           x['97.5%', par_name], ')')
-  }
-
-
-  par( bty = 'n',
-       mfcol = c(n_pars, n_pars + 1L),
-       mar = c(2.5,2.5,1.5,0),
-       mgp = c(1.5, 0.5, 0),
-       oma = c(1,1,1,1))
-
+  par(bty = "n",
+      mfcol = c(n_pars, n_pars + 1L),
+      mar = c(2.5,2.5,1.5,0),
+      mgp = c(1.5, 0.5, 0),
+      oma = c(1,1,1,1))
 
   for (i in seq_len(n_pars)) {
     for(j in seq_len(n_pars)) {
-
-      if (i == j) { # plot hists on diagonal
+      if (i == j) {
+        # plot hists on diagonal
         par_name <- par_names[i]
         bs <- breaks[[par_name]]
-        plot(x = bs[1] ,  # force date axis where needed
+        plot(x = bs[1],
              y = 1,
-             type = 'n',
+             type = "n",
              xlim = c(bs[1], bs[length(bs)]),
              ylim = hist_ylim[[par_name]],
              xlab = par_name,
-             ylab = '',
-             main = print_summ(par_name),
+             ylab = "",
+             main = print_summ(par_name, summ),
              cex.main = 1,
              font.main = 1
         )
@@ -628,20 +538,20 @@ plot.pmcmc_list <- function(x, burn_in = 1, ...) {
         mapply(FUN = function(h, col) {
           plot_hists(h = h,
                      col = col,
-                     breaks = bs)
-        },
+                     breaks = bs)},
                h = hists[[par_name]],
                col = cols_trace)
 
-
-      } else if (i < j) {  # plot correlations on lower triangle
+      } else if (i < j) {
+        # plot correlations on lower triangle
         plot(x = master_chain[[i]],
              y = master_chain[[j]],
              xlab = par_names[i],
              ylab = par_names[j],
              col = cols,
              pch = 20)
-      } else if (i > j) { # print rho on upper triangle
+      } else if (i > j) {
+        # print rho on upper triangle
         plot.new()
         text(x = 0.5,
              y=0.5, cex = 1.5,
@@ -651,16 +561,12 @@ plot.pmcmc_list <- function(x, burn_in = 1, ...) {
     }
   }
 
-
   # print traces in final column
   n_iter <- nrow(master_chain) / n_chains
 
   mapply(FUN = function(par_name, leg) {
 
     trace <- do.call(cbind, traces[[par_name]])
-    if(par_name == "start_date") {
-      trace <- lubridate::as_date(trace, origin = "1970-01-01")
-    }
     matplot(x = seq_len(nrow(trace)),
             y = trace,
             type = "l",
@@ -679,5 +585,18 @@ plot.pmcmc_list <- function(x, burn_in = 1, ...) {
   },
   par_name = par_names,
   leg = c(TRUE, rep(FALSE, length(par_names) - 1)))
+}
 
+plot_hists <- function(h, col, breaks) {
+  with(h, lines(x =  breaks,
+                y = c(density,
+                      density[length(density)]),
+                type = 's',
+                col = col))
+}
+
+plot_traces <- function(trace, col) {
+  lines(x = seq_along(trace),
+        y = trace,
+        col = col)
 }
--- tests/testthat/test-pmcmc.R
+++ tests/testthat/test-pmcmc.R
@@ -12,15 +12,15 @@ test_that("MCMC can run", {
                  'gamma' = function(pars) log(1e-10))
   proposal_kernel <- diag(nrow(range)) * 0.01^2
   row.names(proposal_kernel) <- colnames(proposal_kernel) <- range$name
-  
+
   dat <- example_sir()
   p <- particle_filter$new(dat$data, dat$model, dat$compare)
   n_particles <- 10
   n_mcmc <- 100
-  
+
   # A single chain
   n_chains <- 1
-  X <- pmcmc(range, lprior, p, n_particles, n_mcmc, proposal_kernel, 
+  X <- pmcmc(range, lprior, p, n_particles, n_mcmc, proposal_kernel,
              n_chains = n_chains)
   expect_equal(class(X), "mcstate_pmcmc")
   expect_equal(dim(X$results), c(n_mcmc + 1L, 5))
@@ -30,27 +30,27 @@ test_that("MCMC can run", {
   expect_true(all(X$acceptance_rate >= 0 & X$acceptance_rate <= 1))
   expect_true(all(X$ess >= 0))
 
-  #plot(X)
-  #summary(X)
-  
+  summary(X)
+  plot(X)
+
   # Multiple chains
   n_chains <- 3
   burn_in <- 10
-  Y <- pmcmc(range, lprior, p, n_particles, n_mcmc, proposal_kernel, 
+  Y <- pmcmc(range, lprior, p, n_particles, n_mcmc, proposal_kernel,
              n_chains = n_chains)
   expect_equal(class(Y), "mcstate_pmcmc_list")
   expect_equal(nrow(Y$rhat$psrf), nrow(range))
   expect_length(Y$chains, 3)
-  
-  Y_master <- create_master_chain(Y, burn_in = burn_in)
-  expect_equal(class(Y_master), "mcstate_pmcmc_chain")
-  expect_length(Y_master, 5)
-  expect_length(Y_master$beta, (n_mcmc + 1L - burn_in) * n_chains)
 
+  Y_master <- create_master_chain(Y, burn_in = burn_in)
+  expect_equal(ncol(Y_master), 5)
+  expect_equal(nrow(Y_master), (n_mcmc + 1L - burn_in) * n_chains)
+
+  summary(Y)
+  plot(Y)
 })
 
 test_that("MCMC can infer the correct parameters", {
-  skip("Test not yet complete")
   range <- data.frame(name = c("beta", "gamma"),
                       init = c(0.2, 0.1),
                       min = c(0, 0),
@@ -62,17 +62,17 @@ test_that("MCMC can infer the correct parameters", {
                  'gamma' = function(pars) log(1e-10))
   proposal_kernel <- diag(nrow(range)) * 0.01^2
   row.names(proposal_kernel) <- colnames(proposal_kernel) <- range$name
-  
+
   dat <- example_sir()
   p <- particle_filter$new(dat$data, dat$model, dat$compare)
-  n_particles <- 10
-  n_mcmc <- 1000
+  n_particles <- 20
+  n_mcmc <- 500
   n_chains <- 1
 
   set.seed(1)
-  X <- pmcmc(range, lprior, p, n_particles, n_mcmc, proposal_kernel, 
+  X <- pmcmc(range, lprior, p, n_particles, n_mcmc, proposal_kernel,
              n_chains = n_chains)
 
-  expect_lt(abs(mean(X$results$beta[500:1000]) - 0.2), 0.01)
-  expect_lt(abs(mean(X$results$gamma[500:1000]) - 0.1), 0.01)
+  expect_lt(abs(mean(X$results$beta) - 0.2), 0.01)
+  expect_lt(abs(mean(X$results$gamma) - 0.1), 0.01)
 })
