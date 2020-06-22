e876d2da4e72326bfffae31b5c4f18d60f363e28
John Lees <lees.john6@gmail.com>
2020-06-22 10:56:19 +0100

    Update zero boundary function

--- R/grid_search.R
+++ R/grid_search.R
@@ -34,7 +34,7 @@ grid_search <- function(range, filter, n_particles, tolerance=1E-2) {
   renorm_mat_ll <- prob_matrix / sum(prob_matrix)
 
   if (zero_boundary(renorm_mat_ll, tolerance = tolerance)) {
-    warning("Edges of the probability matrix not zero, check search range")
+    message("Edges of the probability matrix not zero, check search range")
   }
 
   results <- list(
@@ -104,48 +104,12 @@ plot.mcstate_scan <- function(x, ..., what = "likelihood", title = NULL) {
   }
 }
 
-# from https://stackoverflow.com/a/14502298
-##' Extract elements from an array when its dimensions are not known
-##' in advance
-##'
-##'
-##' @param x array
-##' @param dimension which dimension to pull
-##' @param value along the dimension \code{dimension}, which value to
-##' extract.
-##' @param drop logical. If TRUE the result is coerced to the lowest
-##' possible dimension. Recommend setting this to FALSE
-##' @return the appropriate edge of the aray
-##' @details As an axample, if the dimensions of x are 3, 4, 5 then
-##' \code{index_array(x, dimension = 1, value = 2)} will return
-##' x[2, , ]
-index_array <- function(x, dimension, value, drop = FALSE) {
-  # Create list representing arguments supplied to [
-  # bquote() creates an object corresponding to a missing argument
-  indices <- rep(list(bquote()), length(dim(x)))
-  indices[[dimension]] <- value
-
-  # Generate the call to [
-  call <- as.call(c(
-    list(as.name("["), quote(x)),
-    indices,
-    list(drop = drop)
-  ))
-  # Print it, just to make it easier to see what's going on
-
-  # Finally, evaluate it
-  eval(call)
-}
-
+# 2D only - checks edges of an array are less than a tolerance
 zero_boundary <- function(array, tolerance) {
-  correct_range <- 1
-  ndims <- length(dim(array))
-  for (dimension in seq_len(ndims)) {
-    correct_range <- correct_range &&
-      (
-        all(index_array(array, dimension, 1) < tolerance &
-          all(index_array(array, dimension, dim(array)[dimension]) < tolerance))
-      )
-  }
-  correct_range
+  zero_edges <- 
+    all(abs(array[1, ]) < tolerance) &&
+    all(abs(array[, 1]) < tolerance) &&
+    all(abs(array[nrow(array), ]) < tolerance) &&
+    all(abs(array[, ncol(array)]) < tolerance)
+  zero_edges
 }
