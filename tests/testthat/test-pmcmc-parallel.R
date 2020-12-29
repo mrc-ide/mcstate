227846ceb424915dd0e93bdae3fac10a58fcb6ef
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2020-12-29 16:26:52 +0000

    Expand testing

--- tests/testthat/test-pmcmc-parallel.R
+++ tests/testthat/test-pmcmc-parallel.R
@@ -4,33 +4,30 @@ test_that("basic parallel operation", {
   dat <- example_sir()
   n_particles <- 42
   n_steps <- 30
+  n_chains <- 3
 
   p0 <- particle_filter$new(dat$data, dat$model, n_particles, dat$compare,
                             index = dat$index, seed = 1L)
-  s <- make_seeds(2L, 1L)
-
-  obj <- orchestrator$new(dat$pars, p0, n_steps, n_chains = 2,
+  obj <- orchestrator$new(dat$pars, p0, n_steps, n_chains = n_chains,
                           n_workers = 2, n_steps_each = 20)
   while (!obj$step()) {
   }
   ans <- obj$get_results()
 
   ## Run two chains manually with a given pair of seeds:
-  s <- make_seeds(2L, 1L)
-  set.seed(s[[1]]$r)
-  p1 <- particle_filter$new(dat$data, dat$model, n_particles, dat$compare,
-                            index = dat$index, seed = s[[1]]$dust)
-  res1 <- pmcmc(dat$pars, p1, n_steps)
-  set.seed(s[[2]]$r)
-  p2 <- particle_filter$new(dat$data, dat$model, n_particles, dat$compare,
-                            index = dat$index, seed = s[[2]]$dust)
-  res2 <- pmcmc(dat$pars, p2, n_steps)
+  s <- make_seeds(n_chains, 1L)
+  f <- function(idx) {
+    set.seed(s[[idx]]$r)
+    p <- particle_filter$new(dat$data, dat$model, n_particles, dat$compare,
+                              index = dat$index, seed = s[[idx]]$dust)
+    pmcmc(dat$pars, p, n_steps)
+  }
 
-  ## Combined set:
-  res3 <- pmcmc_combine(res1, res2)
+  samples <- lapply(seq_along(s), f)
+  cmp <- pmcmc_combine(samples = samples)
 
-  expect_equal(res3$pars, ans$pars)
-  expect_equal(res3, ans)
+  expect_equal(cmp$pars, ans$pars)
+  expect_equal(cmp, ans)
 })
 
 
@@ -52,4 +49,18 @@ test_that("make seeds with long raw retains size", {
   expect_setequal(names(s[[1]]), c("dust", "r"))
   expect_identical(s[[1]]$dust, seed)
   expect_length(s[[2]]$dust, length(seed))
+})
+
+
+test_that("Don't run with fewer chains than workers", {
+  dat <- example_sir()
+  n_particles <- 42
+  n_steps <- 30
+  p0 <- particle_filter$new(dat$data, dat$model, n_particles, dat$compare,
+                            index = dat$index, seed = 1L)
+  expect_error(
+    obj <- orchestrator$new(dat$pars, p0, n_steps, n_chains = 2,
+                            n_workers = 5, n_steps_each = 20),
+    "'n_chains' (2) is less than 'n_workers' (5)",
+    fixed = TRUE)
 })
