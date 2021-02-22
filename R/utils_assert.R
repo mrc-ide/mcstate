d42de6c9d99582bbeebc87ea286653766279c2ee
Raphael <raphaelsonabend@gmail.com>
2021-02-22 12:43:07 +0000

    add assert_list

--- R/utils_assert.R
+++ R/utils_assert.R
@@ -88,3 +88,16 @@ assert_scalar_character <- function(x, name = deparse(substitute(x))) {
   assert_character(x, name)
   invisible(x)
 }
+
+assert_list <- function(x, class = NULL, name = deparse(substitute(x))) {
+  if (!(is.list(x))) {
+    stop(sprintf("'%s' must be a list", name), call. = FALSE)
+  }
+  if (!is.null(class)) {
+    if (!all(vlapply(x, inherits, what = class))) {
+      stop(sprintf("Elements of '%s' must be in '%s'", name,
+           str_collapse(class)), call. = FALSE)
+    }
+  }
+  invisible(x)
+}
